c3fbd884adccc88d45244d2e17a168eb14bc57a0
jlinick <Justin.P.Linick@jpl.nasa.gov>
2018-09-26 10:02:49 -0700

    update kml generation

--- tosca/services/kml.py
+++ tosca/services/kml.py
@@ -1,5 +1,6 @@
-import os, json, requests
+import os, json, requests, math, re
 from datetime import datetime
+import dateutil.parser
 from flask import jsonify, Blueprint, request, url_for, Response
 from flask_login import login_required
 from pprint import pformat
@@ -31,92 +32,243 @@ def get_kml(dataset=None):
     app.logger.info("source_b64: {}".format(source_b64))
 
     # query
-    es_url = app.config['ES_URL']
-    index = dataset
-    r = requests.post('%s/%s/_search?search_type=scan&scroll=10m&size=100' % (es_url, index), data=source)
-    if r.status_code != 200:
-        app.logger.debug("Failed to query ES. Got status code %d:\n%s" % 
-                         (r.status_code, json.dumps(result, indent=2)))
-    r.raise_for_status()
-    #app.logger.debug("result: %s" % pformat(r.json()))
-
-    scan_result = r.json()
-    count = scan_result['hits']['total']
-    scroll_id = scan_result['_scroll_id']
-
-    # get list of results
-    results = []
-    while True:
-        r = requests.post('%s/_search/scroll?scroll=10m' % es_url, data=scroll_id)
-        res = r.json()
-        scroll_id = res['_scroll_id']
-        if len(res['hits']['hits']) == 0: break
-        for hit in res['hits']['hits']:
-            del hit['_source']['city']
-            results.append(hit['_source'])
-    app.logger.info("results: {}".format(json.dumps(results, indent=2)))
+    decoded_query = json.loads(source)
+    results = get_es_results(query=decoded_query, source='bos')
 
     # build kml
-    kml = simplekml.Kml()
-    kml.document.name = "Acquisitions"
-    kml.document.description
-    for res in results:
-        id = res['id']
-        url = res['urls'][0] if len(res['urls']) > 0 else None
-        browse_url = res['browse_urls'][0] if len(res['browse_urls']) > 0 else None
-        location = res['location']
-        bbox = res['metadata']['bbox']
-        #app.logger.debug("%s" % json.dumps(res['metadata'], indent=2))
-        img_coords = res['metadata'].get('imageCorners', {})
-        if len(img_coords) == 0:
-            if 'dfdn' in res['metadata']:
-                if res['metadata']['direction'] == 'asc':
-                    lowerleft  = res['metadata']['dfdn']['GeoCoordTopLeft']
-                    lowerright = res['metadata']['dfdn']['GeoCoordTopRight']
-                    upperleft  = res['metadata']['dfdn']['GeoCoordBottomLeft']
-                    upperright = res['metadata']['dfdn']['GeoCoordBottomRight']
-                else:
-                    upperright  = res['metadata']['dfdn']['GeoCoordTopLeft']
-                    upperleft = res['metadata']['dfdn']['GeoCoordTopRight']
-                    lowerright  = res['metadata']['dfdn']['GeoCoordBottomLeft']
-                    lowerleft = res['metadata']['dfdn']['GeoCoordBottomRight']
-            else:
-                #img_coords['minLon'] = bbox[2][1]
-                #img_coords['maxLon'] = bbox[1][1]
-                #img_coords['minLat'] = bbox[0][0]
-                #img_coords['maxLat'] = bbox[3][0]
-                #lowerleft  = [img_coords['minLat'], img_coords['minLon'], 0.]
-                #lowerright = [img_coords['minLat'], img_coords['maxLon'], 0.]
-                #upperright = [img_coords['maxLat'], img_coords['maxLon'], 0.]
-                #upperleft  = [img_coords['maxLat'], img_coords['minLon'], 0.]
-                lowerleft  = [bbox[0][0], bbox[0][1], 0.]
-                lowerright  = [bbox[1][0], bbox[1][1], 0.]
-                upperright  = [bbox[2][0], bbox[2][1], 0.]
-                upperleft  = [bbox[3][0], bbox[3][1], 0.]
-        else:
-            lowerleft  = [img_coords['minLat'], img_coords['minLon'], 0.]
-            lowerright = [img_coords['minLat'], img_coords['maxLon'], 0.]
-            upperright = [img_coords['maxLat'], img_coords['maxLon'], 0.]
-            upperleft  = [img_coords['maxLat'], img_coords['minLon'], 0.]
-        ground = kml.newgroundoverlay(name=id)
-        ground.description = id
-        #if location['type'] == 'polygon':
-        #    ground.gxlatlonquad.coords = location['coordinates'][0]
-        ground.gxlatlonquad.coords = [
-            (lowerleft[1], lowerleft[0]),
-            (lowerright[1], lowerright[0]),
-            (upperright[1], upperright[0]),
-            (upperleft[1], upperleft[0])
-        ]
-
-        # add timespan
-        ground.timespan.begin = res['starttime']
-        ground.timespan.end = res['endtime']
-
-        if browse_url:
-            ground.icon.href = '%s/browse_small.png' % browse_url
-        
+    kml_obj = gen_kml(results)
+    
+    # return result
     fname = "sar_availability-acquisitions-{}.kml".format(datetime.utcnow().strftime('%Y%m%dT%H%M%S'))
-    return Response(kml.kml(), headers={'Content-Type': 'application/vnd.google-earth.kml+xml',
+    return Response(kml_obj, headers={'Content-Type': 'application/vnd.google-earth.kml+xml',
                                         'Content-Disposition': 'attachment; filename={}'.format(fname)})
-    #return jsonify({'results': results})
+
+
+def gen_poly(kmlobj, acq):
+    '''Create a new polygon for the KML for an acquisition'''
+    #generate params from acquisition
+    prm = gen_acq_dict(acq)
+    #save the params as a polygon
+    pol = kmlobj.newpolygon(name = prm['name'])
+    pol.outerboundaryis = prm['coord']
+    #pol.innerboundaryis = coord
+    pol.timespan.begin=prm['starttime']
+    pol.timespan.end=prm['endtime']
+    pol.style.linestyle.color = gen_color(acq)
+    pol.style.linestyle.width = 1
+    pol.style.polystyle.color = simplekml.Color.changealphaint(100, gen_color(acq))
+    pol.description = gen_kml_bubble(prm)
+
+def gen_acq_dict(acq):
+    '''returns a dict of acquisition metadata & handles both ESA & BOS SARCAT datatypes'''
+    uid = re.sub('\_+', '_', acq['_id'])
+    coordinates = acq["_source"]["location"]["coordinates"][0]
+    coord = convert_coord(coordinates)
+    platform = walk(acq, 'platform')
+    download_url = walk(acq, 'download_url')
+    name = walk(acq, 'title')
+    if name:
+        name = re.sub('\_+', '_', name)
+    if name is None:
+        name = uid.replace('-bos_sarcat-predicted', '').replace('-', '_').replace('acquisition_', '').replace('Sentinel_1', 'S1')
+    start = walk(acq, 'starttime')
+    end = walk(acq, 'endtime')
+    source = walk(acq, 'source')
+    starttime = dateutil.parser.parse(start).strftime('%Y-%m-%d')
+    endtime = dateutil.parser.parse(end).strftime('%Y-%m-%d')
+    if dateutil.parser.parse(start) > dateutil.parser.parse(end):
+        #end = (dateutil.parser.parse(start) + datetime.timedelta(seconds=60)).strftime('%Y-%m-%dT%H:%M:%SZ')
+        end = start
+        endtime = starttime
+    track = walk(acq, 'trackNumber')
+    location = walk(acq, 'continent')
+    status = walk(acq, 'status')
+    if status:
+        status.replace('\n','')
+    center = walk(acq, 'center')
+    center_str = None
+    if center:
+        center_str = '{:.1f}, {:.1f}'.format(center['coordinates'][1], center['coordinates'][0])
+    #try:
+    location = get_loc_string(acq)
+    #except Exception, err:
+    #    raise Exception(err)
+    #name = acq['_source']['metadata']['title']
+    #platform = acq['_source']['metadata']['platform']
+    orbitnum = walk(acq, 'orbitNumber')
+    dct = {'uid': uid, 'coord': coord, 'coordinates': coordinates, 'start': start, 'end': end, 'starttime': starttime, 'endtime': endtime,
+           'track': track, 'source': source, 'platform': platform, 'orbitnum': orbitnum, 'name': name, 'download_url': download_url,
+           'center': center_str, 'location': location, 'status': status}
+    return dct
+
+def gen_color(acq):
+    '''returns color based on acquisition source'''
+    platform = walk(acq, 'platform')
+    if platform is None:
+        return simplekml.Color.blue
+    platform = platform.lower()
+    if platform.find('sentinel') != -1:
+        return simplekml.Color.white
+    elif platform.find('alos') != -1:
+        return simplekml.Color.green
+    elif platform.find('csk') != -1:
+        return simplekml.Color.blue
+    elif platform.find('radarsat') != -1:
+        return simplekml.Color.red
+    return convert_str_to_color(platform)
+
+def convert_str_to_color(instr):
+    '''converts an input string into simplekml color deterministically'''
+    hexstr = hashlib.md5(instr).hexdigest()
+    r = int(hexstr[0], 16) * int(hexstr[1], 16)
+    g = int(hexstr[2], 16) * int(hexstr[3], 16)
+    b = int(hexstr[4], 16) * int(hexstr[5], 16)
+    d = 255 - max([r, g, b])
+    r += d
+    g += d
+    b += d
+    return simplekml.Color.rgb(r, g, b)
+
+
+def get_loc_string(acq):
+    '''determines the location from the acquisition metadata and returns it as a string'''
+    city_list = walk(acq, 'city')
+    center = walk(acq, 'center')
+    lat = center['coordinates'][1]
+    lon =  center['coordinates'][0]
+    center = [float(lon), float(lat)]
+    loc_str = None
+    distance = None
+    for item in city_list:
+        city_point = [float(item['longitude']),float(item['latitude'])]
+        cur_dist = get_distance(center, city_point)
+        if loc_str is None:
+            distance = cur_dist
+            #build region
+            if item['admin2_name']:
+                loc_str = build_loc_name(item)
+        else:
+            if cur_dist < distance:
+                #build region
+                loc_str = build_loc_name(item)
+    return loc_str
+
+def build_loc_name(item):
+    '''builds the location name from the item'''
+    local = item['admin2_name']
+    region = item['admin1_name']
+    country = item['country_name']
+    if local:
+        loc_str = '{}, {}, {}'.format(local.encode('ascii', 'ignore'), region.encode('ascii', 'ignore'), country.encode('ascii', 'ignore'))
+    else:
+        loc_str = '{}, {}'.format(region.encode('ascii', 'ignore'), country.encode('ascii', 'ignore'))
+    return loc_str
+
+def get_distance(point1, point2):
+    '''returns the distance between the two points in kilometers'''
+    distance = math.acos(math.sin(math.radians(point1[1]))*math.sin(math.radians(point2[1]))+math.cos(math.radians(point1[1]))*math.cos(math.radians(point2[1]))*math.cos(math.radians(point2[0])-math.radians(point1[0])))*6371
+    return distance
+
+
+def gen_kml_bubble(dct):
+    '''generates the html for the kml polygon'''
+    lst = ['name', 'platform', 'location',  'start', 'end', 'source', 'track', 'orbitnum',  'coordinates', 'uid', 'status', 'download_url']
+    outstr = '<table>'
+    for item in lst:
+        if dct[item]:
+            if item == 'download_url':
+                outstr += '<tr><td><b><font color=blue>{}</font></b></td><td> <a href="{}">{}</a></td></tr>'.format(item.capitalize(), dct[item], dct[item])
+            else:
+                outstr += '<tr><td><b><font color=blue>{}</font></b></td><td>{}</td></tr>'.format(item.capitalize(), dct[item])
+    return outstr + '</table>'
+
+def convert_coord(es_coord):
+    '''gen coords for kml'''
+    coord = []
+    for point in es_coord:
+        coord.append((str(point[0]), str(point[1])))
+    return coord
+
+def gen_kml(acquisitions_list, verbose=False):
+    '''Create a KML file showing acquisition coverage'''
+    kmlobj = simplekml.Kml()
+    coord = None
+    for acquisition in acquisitions_list:
+        gen_poly(kmlobj, acquisition)
+    return(kmlobj.kml())
+
+
+def query_es(query, url):
+    '''query elastic search'''
+    iterator_size = query['size']
+    data = json.dumps(query, indent=2)
+    response = requests.get(url, data=data, verify=False, timeout=15)
+    response.raise_for_status()
+    results = json.loads(response.text, encoding='ascii')
+    results_list = results['hits']['hits']
+    total_results = int(results['hits']['total'])
+    if result_count > iterator_size:
+        for i in range(iterator_size, total_results, iterator_size):
+            query['from'] = i
+            response = requests.get(url, data=data, verify=False, timeout=15)
+            response.raise_for_status()
+            results = json.loads(response.text, encoding='ascii')
+            results_list.extend(results['hits']['hits'])
+    return results_list
+
+def walk(node, key_match):
+    '''recursive node walk, returns None if nothing found, returns the value if a key matches key_match'''
+    if isinstance(node, dict):
+        for key, item in node.items():
+            #print('searching {} for {}'.format(key, key_match))
+            if str(key) == str(key_match):
+                #print('found {}: {}'.format(key, item))
+                return item
+            result = walk(item, key_match)
+            if not result is None:
+                return result
+        return None
+    if isinstance(node,list):
+        for item in node:
+            if isinstance(item, dict) or isinstance(item, list):
+                result = walk(item, key_match)
+                if not result is None:
+                    return result
+        return None
+    return None
+
+def get_es_results(query=None, source="bos", verbose=False):
+    '''get the elasticsearch results from the given query'''
+    # first we get relevant values of coordinates & datetimes (or AOI) from the input query
+    location = walk(query, 'location')
+    starttime = walk(query, 'starttime')
+    endtime = walk(query, 'endtime')
+    if starttime != None and type(starttime) != str:
+        endtime = starttime['to']
+        starttime = starttime['from']
+    # run using esa or bos
+    if source == "bos":
+        product = 'acquisition-SARCAT'
+        index = 'grq_v0.2_acquisition-sarcat'
+    elif source == "esa":
+        product = 'acquisition-S1-IW_SLC'
+        index = 'grq_v1.1_acquisition-s1-iw_slc'
+    grq_ip = app.config['ES_URL']
+    url = '{}/{}/_search'.format(grq_ip, index)
+    if verbose:
+        print('using:\nstarttime: {}\nendtime: {}\nlocation: {}\n'.format(starttime,endtime,location))
+    new_query = {"from" : 0,"size" : 500,"query":{"filtered": {"query": {"bool": {"must":[
+                                                                 {"term": {"dataset.raw": product}},
+                                                                 {"range": {"starttime": {"gte": starttime}}},
+                                                                 {"range": {"endtime": {"lte": endtime}}}
+                         ]}},"filter": {"geo_shape":  {"location": location}}}}}
+    if endtime is None:
+        del new_query['query']['filtered']['query']['bool']['must'][2]
+    if starttime is None:
+        del new_query['query']['filtered']['query']['bool']['must'][1]
+    # now run the es query & return the results
+    if verbose:
+        print('query: {}'.format(new_query))
+    results = query_es(new_query, url)
+    return results
+
