1e6a1c46dfb34c8318c165773d01bf90e413a4fe
Gerald Manipon <pymonger@gmail.com>
2018-11-20 20:23:22 +0000

    ignore status code 400 if user_rules already created

--- scripts/create_user_rules_index.py
+++ scripts/create_user_rules_index.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+from requests.exceptions import HTTPError
+
 from tosca import app
 from tosca.services.user_rules import create_user_rules_index
 
-
-create_user_rules_index(app.config['ES_URL'], app.config['USER_RULES_INDEX'])
+try:
+    create_user_rules_index(app.config['ES_URL'], app.config['USER_RULES_INDEX'])
+except HTTPError as e:
+    if e.response.status_code == 400: pass
+    else: raise
