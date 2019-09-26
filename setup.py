a9e0a36cab0e119dd4da60ce6615bbe18e097077
Gerald Manipon <pymonger@gmail.com>
2019-09-26 08:03:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tosca',
-    version='0.3.3',
+    version='0.3.4',
     long_description='Advanced FacetView User Interface',
     packages=find_packages(),
     include_package_data=True,
